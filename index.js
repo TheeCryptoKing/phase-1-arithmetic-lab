097fd5f503763f156a8f96bf1739c609a07502e8
kwamebrowne <theeecryptoking@gmail.com>
2023-04-12 23:09:28 -0500

    arithmetic lab finished

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+/*You were overthinking it 
+function multiply(num1 * num2){
+    return
+}
+function random(x) {
+    Math.random(x)
+    return
+}
+function mod() {
+}
+function max() {
+}*/
+//1. 
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+//2. 
+let random = 2
+Math.random(random)
+//3.
+let num3 = 10
+let num4 = 6
+let mod = num3 % num4
+
+
+//4.
+let max = (5,10,15,20)
+Math.max(max)
